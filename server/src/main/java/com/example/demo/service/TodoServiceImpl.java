2022ea564ded8550c12983952343052def09893c
양혜빈 <skdisk7368@naver.com>
2021-08-23 15:17:25 +0900

    todo 조회 수정

--- server/src/main/java/com/example/demo/service/TodoServiceImpl.java
+++ server/src/main/java/com/example/demo/service/TodoServiceImpl.java
@@ -4,8 +4,10 @@ import com.example.demo.domain.entity.Todo;
 import com.example.demo.domain.repository.TodoRepository;
 import com.example.demo.dto.TodoDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
 
@@ -18,17 +20,17 @@ public class TodoServiceImpl implements TodoService {
     // 모든 todo 가져오기
     @Override
     public List<Todo> searchAllTodo() {
-        return todoRepository.findAll();
+        return todoRepository.findAll(Sort.by(Sort.Direction.DESC, "created"));
     }
 
     // todo 추가
-    @Override
+    @Transactional
     public Todo registerTodo(TodoDTO todoDTO) {
         return todoRepository.save(todoDTO.toEntity());
     }
 
     // Todo 수정
-    @Override
+    @Transactional
     public Todo updateTodo(Long id, String newContent) {
         Todo todo = todoRepository.findById(id).get();
         todo.setContent(newContent);
@@ -36,7 +38,7 @@ public class TodoServiceImpl implements TodoService {
     }
 
     // Todo 삭제
-    @Override
+    @Transactional
     public void deleteTodo(Long id) {
         Todo todo = todoRepository.findById(id).get();
         todoRepository.delete(todo);
